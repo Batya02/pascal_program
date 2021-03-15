1644eaaad304624056b4531c6dd8782ba766b613
Batya02 <magomedov.abdul20012@gmail.com>
2021-03-15 22:57:27 +0300

    initial commit

---
+++ pr10.pas
@@ -0,0 +1,24 @@
+﻿procedure numbers(var l:integer);
+var r,i,m,p,own_num:integer;
+begin 
+  own_num:=9;
+  m:=0;
+  p:=l;
+  while l > 0 do 
+  begin
+    m := m + l mod 10;
+    l := l div 10;
+  end;
+  if m mod own_num = 0 then 
+    writeln('Число ', p, ' делится на ', own_num)
+  else writeln('Число ', p, ' не делится на ', own_num);
+end;
+var a,b,c,d: integer;
+begin 
+  write('Введите 4 целых числа:'); 
+  readln(a,b,c,d);
+  numbers(a);
+  numbers(b);
+  numbers(c);
+  numbers(d);
+end.
---
+++ pr9.pas
@@ -0,0 +1,14 @@
+﻿procedure first_number(var a: integer);
+var r: integer;
+begin 
+  while a > 9 do begin
+    a:= a div 10;
+  end;
+  writeln(a);
+end;
+var a: integer;
+begin 
+  write('Введите число:');
+  readln(a);
+  first_number(a);
+end.
