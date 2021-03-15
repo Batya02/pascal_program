b156e21ed8a443db5caae1ea426122f6bcf64742
Batya02 <magomedov.abdul20012@gmail.com>
2021-03-15 22:58:36 +0300

    initial commit

---
+++ pr11.pas
@@ -0,0 +1,20 @@
+﻿procedure mult(var a: integer);
+var r,m,p:integer;
+begin 
+  m:=1;
+  while a > 0 do 
+  begin
+    m:= m * (a mod 10);
+    a:= a div 10;
+  end;
+  writeln(m);
+end;
+var a,b,c,d:integer;
+begin 
+  write('Введите 4 целых числа:');
+  readln(a,b,c,d);
+  mult(a);
+  mult(b);
+  mult(c);
+  mult(d);
+end.
