22eb3fe82e37ac95a490749d7c774bcd0f2fc6f3
Batya02 <magomedov.abdul20012@gmail.com>
2021-03-17 10:24:29 +0300

    initial message

---
+++ PR10/pr1.pas
@@ -0,0 +1,14 @@
+﻿var a:integer;
+Function factorial(a:integer):integer;
+begin
+  if (a = 0) or (a = 1) then begin 
+  factorial := 1;
+  end
+  else  
+  factorial := a * (factorial(a - 1));
+end;
+begin 
+  write('Введите значение:');
+  readln(a);
+  writeln(factorial(a));
+end.
--- PR9/pr7.pas
+++ PR9/pr7.pas
@@ -1,16 +1,17 @@
 ﻿procedure numbers(var l:integer);
-var r,i,m,p:integer;
+var r,i,m,p,own_num:integer;
 begin 
   m:=0;
   p:=l;
+  own_num:=3;
   while l > 0 do 
   begin
     m := m + l mod 10;
     l := l div 10;
   end;
-  if m mod 3 = 0 then 
-    writeln('Число ', p, ' делится на 3')
-  else writeln('Число ', p, ' не делится на 3');
+  if m mod own_num = 0 then 
+    writeln('Число ', p, ' делится на ', own_num)
+  else writeln('Число ', p, ' не делится на ', own_num);
 end;
 var a,b,c,d: integer;
 begin 
